12b9a65ab5e7ce9518010b6716f7d1068aeb8a78
Luciano Garcia <lualgoritmo@outlook.com>
2022-07-26 12:43:00 -0300

    Fazendo a autenticação

--- app/src/main/java/br/com/zup/marvel/data/repository/MarvelRepository.kt
+++ app/src/main/java/br/com/zup/marvel/data/repository/MarvelRepository.kt
@@ -2,8 +2,24 @@ package br.com.zup.marvel.data.repository
 
 import br.com.zup.marvel.*
 import br.com.zup.marvel.data.model.Marvel
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MarvelRepository {
+    private val auth: FirebaseAuth = Firebase.auth
+    fun cadastreUser(email: String, password: String): Task<AuthResult> {
+        return auth.createUserWithEmailAndPassword(email, password)
+    }
+
+    fun upProfileUser(name: String): Task<Void>? {
+        val profile = UserProfileChangeRequest.Builder().setDisplayName(name).build()
+        return auth.currentUser?.updateProfile(profile)
+    }
+
     fun getMarvelList(): List<Marvel> {
         val marvelList = mutableListOf<Marvel>()
 
